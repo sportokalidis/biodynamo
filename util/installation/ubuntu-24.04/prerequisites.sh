613d64faf24e4cccce891cf7cbb4228eee8dbe78
sportokalidis <sportokalidis98@gmail.com>
2025-06-19 17:26:28 +0200

    Pin cmake version on ubuntu 24.04

--- util/installation/ubuntu-24.04/prerequisites.sh
+++ util/installation/ubuntu-24.04/prerequisites.sh
@@ -41,6 +41,13 @@ sudo apt-add-repository "$REPO"
 sudo apt-get update
 sudo apt install -y cmake-data=3.22.1-1ubuntu1.22.04.1
 
+CMAKE_VER=3.19.3
+CMAKE_SH="cmake-${CMAKE_VER}-linux-x86_64.sh"
+curl -L -O  https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/${CMAKE_SH}
+sudo bash "${CMAKE_SH}" --prefix=/usr/local --skip-license
+rm "${CMAKE_SH}"
+
+
 # Install required packages
 sudo apt-get install -y \
   $(cat $BDM_PROJECT_DIR/util/installation/ubuntu-24.04/package_list_required)
