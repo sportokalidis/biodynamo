8d10fddf2da844887e061f8dbb7ea2e130285477
sportokalidis <sportokalidis98@gmail.com>
2025-09-24 12:36:55 +0200

    Update paraview.cmake

--- cmake/external/ParaView.cmake
+++ cmake/external/ParaView.cmake
@@ -2,17 +2,21 @@ include(utils)
 
 SET(PARAVIEW_SOURCE_DIR "${CMAKE_THIRD_PARTY_DIR}/paraview")
 
-if("$(DETECTED_OS_VERS)" MATCHES "^osx-26" )
-  message(STATUS "Detected osx-26, using osx-15.5 ParaView build")
-  SET(DETECTED_OS_VERS osx-15.5)
+if("${DETECTED_OS_VERS}" MATCHES "^osx-26(\\.|$)")
+  message(STATUS "Detected osx-26, using osx-15.6 ParaView build")
+  set(DETECTED_OS_VERS "osx-15.6")
 endif()
+
+# Debug prints to verify values
+message(STATUS "DETECTED_OS_VERS='${DETECTED_OS_VERS}'")
+message(STATUS "DETECTED_ARCH='${DETECTED_ARCH}'")
 
 if(APPLE AND "${DETECTED_ARCH}" STREQUAL "i386")
   # The release of cmake 3.23.0 broke our build of ParaView on MacOSX. 
   # The build was fixed with a reupload and carries the additional tag cm233.
   SET(PARAVIEW_TAR_FILE paraview_v5.10.0_cm323_${DETECTED_OS_VERS}_default.tar.gz)
 elseif(APPLE AND "${DETECTED_ARCH}" STREQUAL "arm64")
-  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_osx-${DETECTED_OS_VERS}_default.tar.gz)
+  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_${DETECTED_OS_VERS}_default.tar.gz)
 else()
   SET(PARAVIEW_TAR_FILE paraview_v5.9.0_${DETECTED_OS_VERS}_default.tar.gz)
 endif()
